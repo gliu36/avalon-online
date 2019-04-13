be30012dfbff815e7929e1a8634078a5c426f844
Bill Chheu <tuh04091@temple.edu>
2019-04-13 17:50:46 -0400

    added basic backend server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+// console.log server is running
+app.listen(port, () => console.log('Listening on port ${port}'));
+
+app.get('/express_backend', function(req, res) {
+    res.send({express: 'YOUR BACKEND IS CONNECTED'});
+});
+
+
+
