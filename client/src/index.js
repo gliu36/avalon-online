3084fc7d3d7e8833ca368b6c9837ec55e3173224
Bill Chheu <tuh04091@temple.edu>
2019-04-13 18:07:51 -0400

    remove client folder

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Welcome from './components/Welcome';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Welcome />, document.getElementById('root'));
-serviceWorker.unregister();
