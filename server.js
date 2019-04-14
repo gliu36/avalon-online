18448792b797349557595fca5cf81f537ef24cfb
Bill Chheu <tuh04091@temple.edu>
2019-04-13 20:02:26 -0400

    socket

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const server = require('http').Server(app);
+const io = require('socket.io')(http);
 const app = express();
 
 const port = 5000;
+
+let rooms = [];
 
 // console.log server is running
 app.listen(port, () => console.log('Listening on port ${port}'));
@@ -10,5 +14,10 @@ app.get('/express_backend', function(req, res) {
     res.send({express: 'YOUR BACKEND IS CONNECTED'});
 });
 
-
-
+// only for creating a new game
+io.sockets.on('connection', function(socket) {
+    socket.on('room', function(room) {
+        socket.join(room);
+        rooms.push(room);
+    });
+});
